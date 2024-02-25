c6f98185bb0a4f028dd6bab510e9628cc2e59a7c
kindpingwork <kindpingwork@gmail.com>
2024-02-25 14:19:13 +0800

    say hello 範例補完

--- assets/js/say_hello.js
+++ assets/js/say_hello.js
@@ -6,8 +6,20 @@ let dom_response = document.querySelector('#response');
 
 dom_say_btn.addEventListener('click', (e) => {
     let currentName = dom_guest_name.value;
-    dom_response.classList.remove('d-none');
-    dom_response.innerHTML = 'Hi!, ' + currentName;
+    console.log(currentName);
+    // currentName = 'aaaa' => currentName = true 自動轉型為 boolean
+    // if (true) {}
     // TODO: 當 currentName 為空時，該怎麼辦
+    if (currentName) {
+        console.log('Has');
+        dom_response.classList.remove('d-none');
+        dom_response.innerHTML = 'Hi!, ' + currentName;
+    } else {
+        console.log('Not');
+        dom_response.classList.add('d-none');
+    }
+
     // TODO: 顯示完成後，清掉 dom_guest_name 的值，並且將輸入焦點移到此 dom 上面 dom.focus()
+    dom_guest_name.value = '';
+    dom_guest_name.focus();
 });
