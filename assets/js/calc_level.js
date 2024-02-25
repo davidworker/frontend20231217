cf601af12992c2e01c31b8a171628ec20010f2f4
kindpingwork <kindpingwork@gmail.com>
2024-02-25 16:12:46 +0800

    分數轉換等級判斷實作

--- assets/js/calc_level.js
+++ assets/js/calc_level.js
@@ -7,22 +7,75 @@ let dom_response = document.querySelector('#response');
 console.log(dom_num, dom_calc_btn, dom_response);
 
 const transferToLevel = (num) => {
-    let level = '';
+    if (num >= 90) {
+        return '甲'
+    }
 
-    return level;
+    if (num >= 80) {
+        return '乙'
+    }
+
+    if (num >= 70) {
+        return '丙'
+    }
+
+    if (num >= 60) {
+        return '丁'
+    }
+
+    return '不及格';
+}
+
+const checkHasNum = (num) => {
+    if (num) {
+        return true;
+    }
+    return false;
+}
+
+const numInRange = (num) => {
+    if (num >= 0 && num <= 100) {
+        return true;
+    }
+    return false;
+}
+
+// 計算後要將焦點放到分數輸入區塊 [v]
+const focusToNum = () => {
+    // 請實作
 }
 
 
 // 當按下計算按鈕時，抓取數字輸入匡內容
 const calcLevel = (e) => {
+    // 計算前要將顯示區塊隱藏
+    dom_response.classList.add('d-none');
     console.log('start calc.');
     let currentNum = num.value;
     // 印出當前輸入框分數
-    console.log(currentNum);
+    console.log(currentNum, typeof currentNum);
 
     // 檢查是否有輸入分數 [v]
+    if (!checkHasNum(currentNum)) {
+        Swal.fire({
+            title: '轉換錯誤',
+            html: '尚未輸入分數',
+            icon: 'error'
+        })
+        // 計算後要將焦點放到分數輸入區塊 [v]
+        return;
+    }
 
     // 檢查分數是否落在 0 ~ 100 之間 [v]
+    if (!numInRange(currentNum)) {
+        Swal.fire({
+            title: '轉換錯誤',
+            html: `分數: ${currentNum} 未落在 0 ~ 100 之間`,
+            icon: 'error'
+        })
+        // 計算後要將焦點放到分數輸入區塊 [v]
+        return;
+    }
 
     // 計算等級 [v]
     let level = transferToLevel(currentNum);
@@ -30,9 +83,13 @@ const calcLevel = (e) => {
     // 顯示在 dom_response 內，格視為: 您的分數: oo, 等級: xx
     let content = `您的分數: ${currentNum}, 等級: ${level}`;
     dom_response.innerHTML = content;
+    dom_response.classList.remove('d-none');
+    // 計算後要將焦點放到分數輸入區塊 [v]
 }
 
 dom_calc_btn.addEventListener('click', calcLevel);
+
+// 分數輸入區塊按下 enter 也可以計算 [v]
 
 /**
  * 1. 抓取 dom
@@ -42,4 +99,7 @@ dom_calc_btn.addEventListener('click', calcLevel);
  * 5. 檢查分數是否落在 0 ~ 100 之間
  * 6. 計算等級
  * 7. 顯示在 dom_response 內，格視為: 您的分數: oo, 等級: xx
+ * 8. 計算前要將顯示區塊隱藏
+ * 9. 計算後要將焦點放到分數輸入區塊
+ * 10. 分數輸入區塊按下 enter 也可以計算
  */
