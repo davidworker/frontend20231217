cbf7e5be2dec268ca22e63d97ae2bfba50b1cc63
kindpingwork <kindpingwork@gmail.com>
2024-02-25 12:12:00 +0800

    DOM 多選抓取

--- assets/js/dom_basic.js
+++ assets/js/dom_basic.js
@@ -2,4 +2,9 @@ let uid_dom = document.querySelector('#uid');
 console.log(uid_dom);
 
 uid_dom.innerHTML = 'UID update from javascript.';
-uid_dom.style.color = 'blue'
+uid_dom.style.color = 'blue';
+
+let dom_students = document.querySelectorAll('.student');
+console.log(dom_students);
+
+dom_students[0].style.color = 'red';
