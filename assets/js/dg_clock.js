54adfacdfb3e445c68d6397338567c6d60db4b95
kindpingwork <kindpingwork@gmail.com>
2024-03-03 10:40:58 +0800

    電子鐘程式實作

--- assets/js/dg_clock.js
+++ assets/js/dg_clock.js
@@ -5,33 +5,54 @@ let dom_m2 = document.querySelector('#clock-m2');
 let dom_s1 = document.querySelector('#clock-s1');
 let dom_s2 = document.querySelector('#clock-s2');
 
-console.log(dom_h1, dom_h2, dom_m1, dom_m2, dom_s1, dom_s2)
-
-// 取得現在時間
+/**
+ * 取得現在時間
+ * @returns {object}
+ */
 const getCurrentTime = () => {
     let d = new Date();
     let hh = d.getHours();
     let mm = d.getMinutes();
     let ss = d.getSeconds();
 
-    // 秒數小於 10，需自動補 0，確保內容為兩位數
     return {
-        hh: hh,
-        mm: mm,
-        ss: ss
+        hh: paddingNum(hh),
+        mm: paddingNum(mm),
+        ss: paddingNum(ss)
     };
 }
 
-// 更新 UI
-const updateUI = () => {
-    console.log('update ui');
-    let current = getCurrentTime();
-    console.log(current);
-    // 實作將數字更新到對應的 dom 內
+/**
+ * 數字小於 10，前面自動補 0，確保內容為兩位數
+ * @param {int} num 
+ * @returns {string}
+ */
+const paddingNum = (num) => {
+    if (num < 10) {
+        num = `0${num}`;
+    } else {
+        num = num.toString()
+    }
+    return num;
 }
 
-setInterval(() => {
+/**
+ * 更新 UI
+ * @return {void}
+ */
+const updateUI = () => {
+    let current = getCurrentTime();
+    dom_h1.innerHTML = current.hh[0];
+    dom_h2.innerHTML = current.hh[1];
+    dom_m1.innerHTML = current.mm[0];
+    dom_m2.innerHTML = current.mm[1];
+    dom_s1.innerHTML = current.ss[0];
+    dom_s2.innerHTML = current.ss[1];
+}
+
+let sn = setInterval(() => {
     updateUI();
 }, 1000)
 
+// 畫面初始化時執行
 updateUI();
