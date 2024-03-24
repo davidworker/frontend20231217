1a1d51fd4248167cf8544df03e3f0ffdc3b78754
kindpingwork <kindpingwork@gmail.com>
2024-03-24 12:58:11 +0800

    橫幅廣告圖片區塊 介紹雙向綁定 假圖產生

--- vue_project/assets/js/main.js
+++ vue_project/assets/js/main.js
@@ -13,7 +13,7 @@ const options = {
                 enable: 'yes',
                 items: [
                     {
-                        open: false,
+                        open: true,
                         enable: 'yes',
                         img_link: '1',
                         img_link_target: '_blank',
@@ -71,6 +71,12 @@ const options = {
         setEnable(type) {
             if (type == 'top_banner_enable') {
                 this.top_banner.enable = this.top_banner.enable == 'yes' ? 'no' : 'yes';
+            }
+        },
+        async setImage(type, index, field) {
+            let images = ['https://fastly.picsum.photos/id/1080/200/200.jpg?hmac=0okKAdyiW9oTgR5PNZQrDYFtWu7HAt93nI93ZpfelUw', 'https://fastly.picsum.photos/id/1035/200/200.jpg?hmac=IDuYUZQ_7a6h4pQU2k7p2nxT-MjMt4uy-p3ze94KtA4']
+            if (type == 'top_banner') {
+                this.top_banner.items[index][field] = images[Math.round(Math.random() * 100) % 2];
             }
         }
     },
